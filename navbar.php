e1e72ee828cea7996b833f87ecd98d4d53310b4d
ChemaCso <56057878+ChemaCso@users.noreply.github.com>
2019-10-17 10:50:50 -0500

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,14 @@
+<div class="contenedor" id="contenedor-navBar">
+            <img class="logo" src="/img/ScalaBlanco.gif" alt="ScalaNegocios">
+            <i class="fas fa-bars" id="menu-icon"></i>
+            <nav>
+                <ul>
+                    <li><a href="index.php">INICIO</a></li>
+                    <li><a href="nosotros.php">NOSOTROS</a></li>
+                    <li><a href="servicios.php">SERVICIOS</a></li>
+                    <li><a href="contacto.php">CONTACTO</a></li>
+                </ul>
+            </nav>
+</div>
+      
+
---
+++ nosotros.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ScalaNegocios</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700|Roboto:300,400,700&display=swap"
+        rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d00b1f9a63.js"></script>
+</head>
+
+<body>
+    <header id="nosotros">
+        <?php
+        require_once 'navbar.php';
+    ?>
+
+        <div class="contenedor">
+            <div class="contenedor-title">
+                <div class="title">
+                    <h1>¿Quiénes somos?</h1>
+                    <p>Somos una agencia de negocios para emprendedores, empresarios y gobierno, que quieran crear,
+                        desarrollar o mejorar sus empresas.</p>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <section id="proposito">
+            <div class="contenedor" id="contenedor-propositos">
+                    <div class="box">
+                            <h1>Misión</h1>
+                            <p>Brindar a nuestros clientes estrategias de negocios versátiles promoviendo el crecimiento y la innovación continua.</p>
+                    </div>
+                    <div class="box">
+                            <h1>Visión</h1>
+                            <p>Ser una promotora negocios para el desarrollo económico sostenible.</p>
+                    </div>
+                    <div class="box">
+                            <h1>Objetivo</h1>
+                            <p>Ser una promotora negocios para el desarrollo económico sostenible.</p>
+                    </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <?php
+        require_once 'footer.php';
+    ?>
+    </footer>
+
+    <!-- <script src="/js/main.js"></script> -->
+</body>
+
+</html>
