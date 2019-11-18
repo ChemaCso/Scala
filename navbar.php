292758b09c392a5ab6ea39005cb0dbbf7e200cc4
ChemaCso <56057878+ChemaCso@users.noreply.github.com>
2019-11-18 16:31:37 -0600

    prueba menu movil
    Co-Authored-By: null <ChemaCso@users.noreply.github.com>

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <div class="contenedor" id="contenedor-navBar">
             <img class="logo" src="/img/ScalaBlanco.gif" alt="ScalaNegocios">
-            <i class="fas fa-bars" id="menu-icon"></i>
+          <img class="menu-icon" src="/imagenes/iconomenu.png" alt="">
             <nav>
                 <ul>
                     <li><a href="index.php">INICIO</a></li>
