1c489df4a7dfb0226e97dc65d155a8e5355ebb1c
ChemaCso <56057878+ChemaCso@users.noreply.github.com>
2019-11-18 17:45:34 -0600

    menu
    Co-Authored-By: null <ChemaCso@users.noreply.github.com>

--- contacto.php
+++ contacto.php
@@ -7,10 +7,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ScalaNegocios</title>
     <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/cssC/estilos.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700|Roboto:300,400,700&display=swap"
         rel="stylesheet">
     <script src="https://kit.fontawesome.com/d00b1f9a63.js"></script>
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700|Roboto:300,400,700&display=swap"
+        rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d00b1f9a63.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
 
 </head>
 
