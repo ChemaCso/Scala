199697df7b9fdd11f2c5db2964a6b9c174a4e3c1
ChemaCso <56057878+ChemaCso@users.noreply.github.com>
2019-11-18 17:52:45 -0600

    menu
    Co-Authored-By: null <ChemaCso@users.noreply.github.com>

--- navbar.php
+++ navbar.php
@@ -11,3 +11,15 @@
             </nav>
 
 </div>
+
+<script
+src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
+</script>
+
+<script>
+$(document).ready(function(){
+  $('.menu-icon').click(function(){
+    $('header nav').slideToggle();
+  })
+})
+</script>
