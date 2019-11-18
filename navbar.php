25f219e8284c75493a7275853fbb91accd9eb24b
ChemaCso <56057878+ChemaCso@users.noreply.github.com>
2019-11-18 17:14:35 -0600

    menu
    Co-Authored-By: null <ChemaCso@users.noreply.github.com>

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <div class="contenedor" id="contenedor-navBar">
             <img class="logo" src="/img/ScalaBlanco.gif" alt="ScalaNegocios">
-          <img class="menu-icon" src="/imagenes/iconomenu1.png" alt="menus">
+          <img class="menu-icon" src="/imagenes/menu.png" alt="menus">
             <nav>
                 <ul>
                     <li><a href="index.php">INICIO</a></li>
