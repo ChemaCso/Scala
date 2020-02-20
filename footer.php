76e7e237a699912d66b9c61ea3c7cd439f203535
ChemaCso <56057878+ChemaCso@users.noreply.github.com>
2020-02-20 17:57:01 -0600

    num

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<a class="appwhatsapp" href="https://api.whatsapp.com/send?phone=7331053588&text=¡Hola!&nbsp;Me&nbsp;gustaría&nbsp;conocer&nbsp;más&nbsp;sobre&nbsp;ScalaNegocios">
+<a class="appwhatsapp" href="https://api.whatsapp.com/send?phone=+527331053588&text=¡Hola!&nbsp;Me&nbsp;gustaría&nbsp;conocer&nbsp;más&nbsp;sobre&nbsp;ScalaNegocios">
 <img src="img/iconwhatsapp.png" alt="whatsappm">
 </a>
 
